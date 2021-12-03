8741b157196e3c499440b81f33275bfc79113e39
Chenye Zhu <zhuchenye@gmail.com>
2021-12-03 00:57:48 -0800

    allow longer A* run time

--- scripts/planners/P1_astar.py
+++ scripts/planners/P1_astar.py
@@ -7,7 +7,7 @@ class AStar(object):
     """Represents a motion planning problem to be solved using A*"""
 
     def __init__(self, statespace_lo, statespace_hi, x_init, x_goal, occupancy: 'DetOccupancyGrid2D', resolution=1,
-                        time_out_steps = 100):
+                        time_out_steps = 500):
         self.statespace_lo = statespace_lo         # state space lower bound (e.g., [-5, -5])
         self.statespace_hi = statespace_hi         # state space upper bound (e.g., [5, 5])
         self.occupancy = occupancy                 # occupancy grid (a DetOccupancyGrid2D object)
