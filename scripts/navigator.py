e4f5ce678b1a7acbbac7b435dbf6b18bd9155322
Chenye Zhu <zhuchenye@gmail.com>
2021-12-07 18:35:25 -0800

    Update stop sign parameters

--- scripts/navigator.py
+++ scripts/navigator.py
@@ -151,7 +151,7 @@ class Navigator:
         self.stop_time = rospy.get_param("~stop_sign_time", 2.)
 
         # Minimum distance from a stop sign to obey it
-        self.stop_min_dist = rospy.get_param("~stop_min_dist", 0.6)
+        self.stop_min_dist = rospy.get_param("~stop_min_dist", .8)
 
         # Time taken to cross an intersection
         self.crossing_time = rospy.get_param("~crossing_time", 5.)
